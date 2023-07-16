52cd3405d47df99dbcc04a0aa91a9133b5b2acfd
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-07-16 20:35:43 +0200

    Add project files.

---
+++ Tydzien3Lekcja21ZD/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Tydzien3Lekcja21ZD
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				int dayOfBirth, monthOfBirth, yearOfBirth;
+				Console.Write("Podaj imię: ");
+				var name = Console.ReadLine();
+
+				while (true)
+				{
+					try
+					{
+						Console.Write("Podaj dzień urodzenia: ");
+						dayOfBirth = User.GetDayInput();
+
+						break;
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+
+				while (true)
+				{
+					try
+					{
+						Console.Write("Podaj miesiąc urodzenia [od 1 do 12]: ");
+						monthOfBirth = User.GetMonthInput();
+
+						break;
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+
+				while (true)
+				{
+					try
+					{
+						Console.Write("Podaj rok urodzenia: ");
+						yearOfBirth = User.GetYearInput();
+
+						break;
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+
+				Console.Write("Podaj miasto urodzenia: ");
+				var cityOfBirth = Console.ReadLine();
+
+				var user = new User(name, dayOfBirth, monthOfBirth, yearOfBirth, cityOfBirth);
+				user.GetInfo();
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Wystąpił nieoczekiwany błąd, program zostanie zamknięty!");
+			}
+			finally
+			{
+				Console.Write("Aby zakończyć program naciśnij dowolny klawisz...");
+				Console.ReadKey();
+			}
+
+		}
+	}
+}
---
+++ Tydzien3Lekcja21ZD/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Tydzien3Lekcja21ZD
+{
+	internal class User
+	{
+		private string _name { get; set; }
+		private DateTime _dateOfBirth { get; set; }
+		private string _cityOfBirth { get; set; }
+
+		public User(string Name, int DayOfBirth, int MonthOfBirth, int YearOfBirth, string CityOfBirth)
+		{
+			this._name = Name;
+			this._dateOfBirth = new DateTime(YearOfBirth, MonthOfBirth, DayOfBirth);
+			this._cityOfBirth = CityOfBirth;
+		}
+
+		public int GetAge()
+		{
+			DateTime today = DateTime.Now;
+			var age = today.Year - this._dateOfBirth.Year;
+			if (today.DayOfYear < this._dateOfBirth.DayOfYear) age -= 1;
+			if (age < 0) age = 0;
+			return age;
+		}
+
+		public void GetInfo()
+		{
+			Console.WriteLine($"Cześć {this._name}, urodziłeś się w {this._cityOfBirth} i masz {this.GetAge()} lat.");
+		}
+
+		public static int GetDayInput()
+		{
+			if (!(int.TryParse(Console.ReadLine(), out int input)))
+				throw new Exception("Nieprawidłowa wartość!");
+			else
+				if (input < 1 || input > 31) throw new Exception("Nieprawidłowa wartość!");
+			return input;
+		}
+
+		public static int GetMonthInput()
+		{
+			if (!(int.TryParse(Console.ReadLine(), out int input)))
+				throw new Exception("Nieprawidłowa wartość!");
+			else
+				if (input < 1 || input > 12) throw new Exception("Nieprawidłowa wartość!");
+			return input;
+		}
+		public static int GetYearInput()
+		{
+			if (!(int.TryParse(Console.ReadLine(), out int input)))
+				throw new Exception("Nieprawidłowa wartość!");
+			else
+				if (input < 1 || input > DateTime.Now.Year) throw new Exception("Nieprawidłowa wartość!");
+			return input;
+		}
+	}
+
+}
